88a3a1ac98258e2f890e9885b0cb5498d38c4f14
tomhosker <tomdothosker@gmail.com>
2025-04-10 01:28:52 +0100

    Added list of user-generated pages.

--- routes/usergen.js
+++ routes/usergen.js
@@ -7,11 +7,24 @@ const express = require("express");
 
 // Local imports.
 const UsergenORM = require("../lib/orm/usergen_orm.js");
+const ListORM = require("../lib/orm/list_orm.js");
 const Finaliser = require("../lib/finaliser.js");
 
 // Constants.
 const router = express.Router();
 const finaliser = new Finaliser();
+
+// Return a list of all user-generated pages.
+router.get("/", (req, res) => {
+    const orm =
+        new ListORM("UserGeneratedPage", "code", "title", "title", "usergen");
+    let properties;
+
+    orm.gatherDataAsync().then((data) => {
+        properties = {title: "List of User-Generated Pages", data: data};
+        finaliser.protoRender(req, res, "listofpages", properties);
+    });
+});
 
 // Return a given user-generated page.
 router.get("/:id", (req, res) => {
@@ -28,6 +41,5 @@ router.get("/:id", (req, res) => {
         }
     });
 });
-
 
 module.exports = router;
