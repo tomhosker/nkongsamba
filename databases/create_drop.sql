d200ac7828ef99146e36da8e9eff8474b96ab287
tomhosker <tomdothosker@gmail.com>
2025-07-07 20:56:23 +0100

    Fixing cloud database...

--- databases/create_drop.sql
+++ databases/create_drop.sql
@@ -21,5 +21,16 @@ CREATE TABLE UserGeneratedPage (
     code VARCHAR(99) PRIMARY KEY,
     title VARCHAR(99) UNIQUE,
     section VARCHAR(99) NOT NULL DEFAULT 'internal-use-only' REFERENCES UserGeneratedSection (code),
-    html VARCHAR(99999),
+    html VARCHAR(99999)
 );
+
+-- Insert dummy data.
+INSERT INTO UserLoginDetails (id, username, hashed_password)
+VALUES (
+    1,
+    'admin',
+    '84983c60f7daadc1cb8698621f802c0d9f9a3c3c295c810748fb048115c186ec' -- i.e. 'guest'.
+);
+
+INSERT INTO UserGeneratedSection (code, title)
+VALUES ('internal-use-only', 'Internal Use Only');
